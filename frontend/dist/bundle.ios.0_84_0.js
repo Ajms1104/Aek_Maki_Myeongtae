3a3cdab10c2d7cd29afa40a2235c80a9f0a6fb7f
LSjin16 <leesj5692@gmail.com>
2026-07-15 15:44:35 +0900

    fix: change fish banner card aspectRatio to 1.6 to prevent image text clipping

--- frontend/dist/bundle.ios.0_84_0.js
+++ frontend/dist/bundle.ios.0_84_0.js
@@ -9,7 +9,7 @@ var global = typeof globalThis !== 'undefined' ? globalThis : typeof global !== 
     host: ''
   };
   global.__appsInToss = {
-    deploymentId: "019f647f-9f31-7adf-8199-d05ef650f7e8",
+    deploymentId: "019f6484-f1fd-7c01-8b6b-197f9dda1cf9",
     brandDisplayName: "\uC561\uB9C9\uC774AI",
     brandPrimaryColor: "#3182F6",
     brandIcon: "https://static.toss.im/appsintoss/16735/bca00758-7ffe-41cb-9210-0017b194f21e.png",
@@ -132,7 +132,7 @@ if (global.__MICRO_FRONTEND__ == null) {
     host: ''
   };
   global.__appsInToss = {
-    deploymentId: "019f647f-9f31-7adf-8199-d05ef650f7e8",
+    deploymentId: "019f6484-f1fd-7c01-8b6b-197f9dda1cf9",
     brandDisplayName: "\uC561\uB9C9\uC774AI",
     brandPrimaryColor: "#3182F6",
     brandIcon: "https://static.toss.im/appsintoss/16735/bca00758-7ffe-41cb-9210-0017b194f21e.png",
