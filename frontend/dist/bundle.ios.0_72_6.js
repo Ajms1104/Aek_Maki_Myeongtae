d78931ecfc54417b585911387f1c5feecfc05256
LSjin16 <leesj5692@gmail.com>
2026-07-15 17:41:45 +0900

    fix: reset body overflow/position in admin entry for PC scrolling

--- frontend/dist/bundle.ios.0_72_6.js
+++ frontend/dist/bundle.ios.0_72_6.js
@@ -9,7 +9,7 @@ var global = typeof globalThis !== 'undefined' ? globalThis : typeof global !== 
     host: ''
   };
   global.__appsInToss = {
-    deploymentId: "019f64ed-a47e-7c8f-a508-89942436b29b",
+    deploymentId: "019f64f0-0746-7851-8681-c2835a3548c1",
     brandDisplayName: "\uC561\uB9C9\uC774AI",
     brandPrimaryColor: "#3182F6",
     brandIcon: "https://static.toss.im/appsintoss/16735/bca00758-7ffe-41cb-9210-0017b194f21e.png",
@@ -132,7 +132,7 @@ if (global.__MICRO_FRONTEND__ == null) {
     host: ''
   };
   global.__appsInToss = {
-    deploymentId: "019f64ed-a47e-7c8f-a508-89942436b29b",
+    deploymentId: "019f64f0-0746-7851-8681-c2835a3548c1",
     brandDisplayName: "\uC561\uB9C9\uC774AI",
     brandPrimaryColor: "#3182F6",
     brandIcon: "https://static.toss.im/appsintoss/16735/bca00758-7ffe-41cb-9210-0017b194f21e.png",
--- frontend/src/admin.tsx
+++ frontend/src/admin.tsx
@@ -8,6 +8,14 @@ import { UIProvider } from './contexts/UIContext';
 // 🔒 [어드민 독립 배포 엔트리] 토스 네이티브 브릿지 주입 없이 일반 PC 웹 브라우저 표준으로 기동
 console.log('[Admin Init] 어드민 대시보드 콘솔 앱 구동');
 
+// 🖥️ [PC 어드민 body 스타일 초기화]
+// globalStyles.ts에 모바일 앱 전용으로 설정된 overflow:hidden, position:fixed를 PC 환경에 맞게 풀어줍니다.
+document.body.style.overflow = 'auto';
+document.body.style.position = 'static';
+document.body.style.height = 'auto';
+document.body.style.display = 'block';
+document.documentElement.style.overflow = 'auto';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <UIProvider>
